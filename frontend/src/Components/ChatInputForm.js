0bed9b147e9d3b06afaf29f062c7dab0cf8f9147
Jesse1224 <jessekartabani@gmail.com>
2023-01-09 22:23:57 +1100

    Add submit button

--- frontend/src/Components/ChatInputForm.js
+++ frontend/src/Components/ChatInputForm.js
@@ -9,7 +9,25 @@ const ChatInputForm = ({ input, setInput, handleSubmit }) => {
           className="chat-input-textarea"
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          placeholder="Prompt"
         ></input>
+        <button className="submit-button">
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            strokeWidth={0}
+            viewBox="0 0 20 20"
+            className="submit-svg"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10.894 2.553a1 1 0 0 0-1.788 0l-7 14a1 1 0 0 0 1.169 1.409l5-1.429A1 1 0 0 0 9 15.571V11a1 1 0 1 1 2 0v4.571a1 1 0 0 0 .725.962l5 1.428a1 1 0 0 0 1.17-1.408l-7-14z"
+              stroke="none"
+            />
+          </svg>
+        </button>
       </form>
     </div>
   );
