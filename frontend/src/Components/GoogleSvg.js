90505365833904c06b23043d5cf98976173b643a
Jesse1224 <jessekartabani@gmail.com>
2023-01-25 19:38:22 +1100

    re style store and auth buttons

--- frontend/src/Components/GoogleSvg.js
+++ frontend/src/Components/GoogleSvg.js
@@ -3,9 +3,7 @@ import React from "react";
 const GoogleSvg = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    height={24}
-    width={24}
-    style={{ marginRight: 10 }}
+    style={{ marginRight: 10, width: 23, height: 23 }}
   >
     <path
       d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
