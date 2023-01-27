d530659b3f02e46178d709b538214391dce77221
Jesse1224 <jessekartabani@gmail.com>
2023-01-27 17:33:50 +1100

    fix compiler warnings

--- frontend/src/Pages/MainPage.js
+++ frontend/src/Pages/MainPage.js
@@ -43,22 +43,11 @@ function MainPage({ app, db }) {
     setIsLoggingIn(true);
     signInWithRedirect(auth, provider);
     getRedirectResult(auth)
-      .then((result) => {
-        // This gives you a Google Access Token. You can use it to access Google APIs.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-
-        // The signed-in user info.
-        const user = result.user;
+      .then(() => {
+        console.log("Signed In");
       })
       .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.customData.email;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
+        console.log(error.message);
       });
   }
 
