8c042f2c254f3a60da7d2a6bb67f94e5c505d0bb
Jesse1224 <jessekartabani@gmail.com>
2023-01-31 01:30:11 +1100

    remove old tests

--- frontend/src/__tests__/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "../App";
-
-test("App Renders", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/ChatGPT Enhanced/i);
-  expect(linkElement).toBeInTheDocument();
-});
