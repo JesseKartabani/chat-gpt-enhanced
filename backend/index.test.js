62e90d28f8fc3bf74b0723f30bd60cad77a7982f
Jesse1224 <jessekartabani@gmail.com>
2023-02-04 19:31:19 +1100

    update index test

--- backend/index.test.js
+++ backend/index.test.js
@@ -1,14 +1,19 @@
 const request = require("supertest");
 const app = require("./index");
 
-jest.setTimeout(6000);
+describe("POST /", () => {
+  it("Responds with 401 Unauthorized when API key is missing", async () => {
+    const response = await request(app).post("/");
+    expect(response.status).toBe(401);
+    expect(response.body.error).toBe("Unauthorized");
+  });
 
-describe("Backend", () => {
-  test("AI responds with a completion message", async () => {
-    const res = await request(app)
+  it("Responds with 200 and an AI-generated message when API key is valid", async () => {
+    const response = await request(app)
       .post("/")
-      .send({ message: "What is the meaning of life?", temperature: 0.5 });
-    expect(res.statusCode).toBe(200);
-    expect(res.body).toEqual({ message: expect.any(String) });
+      .set("Authorization", `Bearer ${process.env.MY_API_KEY}`)
+      .send({ message: "Hello, how are you?", temperature: 0.5 });
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBeDefined();
   });
 });
